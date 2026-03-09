0bd9813ccbbddd5ef3b2dd1e7ee5040f8c9a6fc3
sofiane tirecht <sofiane.tirecht@gmail.com>
2026-03-09 10:23:49 +0100

    sup de comm

--- routes/restaurantDetail.js
+++ routes/restaurantDetail.js
@@ -3,13 +3,12 @@ var router = express.Router();
 require("../models/connection");
 
 const Restaurant = require("../models/restaurants");
-// Hello encore une fois, je suis désolé pour les erreurs précédentes. Voici le code corrigé pour la route GET /restaurants/:name qui permet de récupérer les détails d'un restaurant par son nom. J'ai ajouté un décodage du nom pour gérer les espaces et les caractères spéciaux, ainsi qu'une recherche insensible à la casse. yolo
 // GET /restaurants/:name → détails d'un restaurant par son nom
 router.get("/:name", async (req, res) => {
   try {
-    // Décodage du nom pour gérer les espaces et caractères spéciaux
+    // gérer les espaces et caractères spéciaux
     const name = decodeURIComponent(req.params.name);
-    // Échapper les caractères spéciaux pour la regex
+    // les caractères spéciaux pour la regex
     const escapedName = name.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
     const restaurant = await Restaurant.findOne({
       name: { $regex: new RegExp(`^${escapedName}$`, "i") },
